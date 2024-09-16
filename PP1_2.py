b4e30cf8ed499e53ed314c9933a7abc7366cfb13
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-09-16 14:10:31 +0000

    Answer

--- PP1_2.py
+++ PP1_2.py
@@ -3,18 +3,45 @@
 #Write Assignment code here
 
 def q1():
+    name_ = "Alex"
+    print(name_)
 
 def q2():
+    low = 2
+    mid = 3
+    high = 5
+
+    print(low + mid + high)
 
 def q3():
 
+    float = 21
+    forr = 4
+    print(float // forr)
+
 def q4():
+
+    big = 6.3
+    small = 3.0
+    print(big / small)
 
 def q5():
 
+    happy = "Hello World"
+    print(happy)
+    happy = "Goodbye World"
+    print(happy)
+
 def q6():
 
+    false = True
+    print(false)
+
 def q7():
+
+    odd = 7
+    even = 2
+    print(odd % even)
 
 q1()
 q2()
